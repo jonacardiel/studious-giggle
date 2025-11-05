67a7d9180537766764426d7f281ba8fd9bc32dfb
jonacardiel <131711417+jonacardiel@users.noreply.github.com>
2025-11-04 17:02:15 -0700

    feat: add ESLint configuration and scripts for linting
    style: update header styles to use grid layout and improve toggle button
    
    fix: implement navigation toggle functionality for global menu
    
    chore: update package.json with devDependencies for ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import pluginJs from "@eslint/js";
+import globals from "globals";
+
+export default [
+  pluginJs.configs.recommended,
+  {
+    languageOptions: { globals: globals.browser },
+    rules: {
+      semi: "error"
+    }
+  }
+];
